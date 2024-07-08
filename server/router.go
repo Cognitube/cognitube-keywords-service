df742af95b9616454c0a81fe66b47dcdbd94fdda
Haozhe Zhang <zhz19980514@gmail.com>
2024-07-08 09:03:50 -0500

    fix: fix api route

--- server/router.go
+++ server/router.go
@@ -7,5 +7,5 @@ import (
 // SetupRoutes configures the HTTP routes for the server.
 func SetupRoutes(r *mux.Router, keywordsService ICognitubeKeywordsService) {
 	keywordsHandler := NewKeywordsHandler(keywordsService)
-	r.HandleFunc("/keywords", keywordsHandler.GetKeywords).Methods("POST")
+	r.HandleFunc("/api/v1/get-keywords", keywordsHandler.GetKeywords).Methods("POST")
 }
