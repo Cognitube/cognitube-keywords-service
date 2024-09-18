315d037cec56a973c22d307e7d0aab6e767a23c9
Haozhe Zhang <zhz19980514@gmail.com>
2024-09-17 21:35:43 -0500

    test: test if code works without callback

--- server/router.go
+++ server/router.go
@@ -11,7 +11,7 @@ func SetupRoutes(r *mux.Router, s *CongitubeAIServer) {
 	keywordsHandler := handler.NewKeywordsHandler(s.keywordsService)
 	r.HandleFunc("/", handler.Home).Methods("GET")
 	r.HandleFunc("/api/v1/get-keywords", keywordsHandler.GetKeywords).Methods("POST") // Sync
-	r.HandleFunc("/api/v1/callback", keywordsHandler.CallbackPost).Methods("POST")    // This callback is broken
+	// r.HandleFunc("/api/v1/callback", keywordsHandler.CallbackPost).Methods("POST")    // This callback is broken
 	r.HandleFunc("/api/v1/transcription/create", keywordsHandler.CreateTranscription).Methods("POST")
 	r.HandleFunc("/api/v1/transcription/process", keywordsHandler.ProcessTranscriptionResult).Methods("POST")
 
